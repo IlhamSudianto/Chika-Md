92da1d8d91df3a151858c95c03fd684214d9e7a5
IlhamSudianto <107314603+IlhamSudianto@users.noreply.github.com>
2023-01-19 20:42:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6283129736549'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6283129736549'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6283129736549' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
@@ -33,16 +33,16 @@ global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://www.youtube.com/channel/UC3MZHmziMKqvXAH5jCg2y1Q'
+global.ig = 'Comingsooon'
+global.mygc = 'https://chat.whatsapp.com/G8zCWAjGSvnHy3RQ2J28os'
+global.myweb = 'Comingsooon'
+global.email = 'reskigaming88@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© 7IT4 BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Reski' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
